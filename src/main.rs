5808ea92431ee275f03622c7f2539564e75b023f
Walden Perry <b0shbbl@hotmail.com>
2026-08-24 20:47:59 -0500

    It works!!!!!!!

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 use axum::{
-    Json, Router, extract::Path, http::StatusCode, routing::{get, post},
+    Json, Router,
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
 };
+use serde::Deserialize;
 use serde_json::{Value, json};
+use std::sync::atomic::{AtomicU64, Ordering};
 
 #[derive(Deserialize)]
 struct CreateCityPayload {
-    city_name: String
+    city_name: String,
 }
 
-static mut GLOBAL_ID: i32 = 0;
+static AUTO_INC: AtomicU64 = AtomicU64::new(1);
 
 #[tokio::main]
 async fn main() {
     println!("Starting Server");
-    
+
     // build our application with a single route
     let app = Router::new()
         .route("/hello", get(hello))
         .route("/data", get(data))
         .route("/bad", get(bad_request))
-        .route("/city", post({
-            move |body| create_city(body)
-        }))
-        .route("/city/{id}", get({
-            move |path| get_city(path)
-        }))
-    ;
+        .route("/city", post(move |body| create_city(body)))
+        .route("/city/{id}", get(move |path| get_city(path)));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:6655").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -54,12 +54,11 @@ async fn get_city(Path(city_id): Path<String>) -> Json<Value> {
 async fn create_city(Json(payload): Json<CreateCityPayload>) -> Result<Json<Value>, StatusCode> {
     if payload.city_name.len() > 8 {
         Err(StatusCode::BAD_REQUEST)
-    }
-    else {
-        unsafe {
-            let id = GLOBAL_ID;
-            GLOBAL_ID += 1;
-        }
+    } else {
+        // https://doc.rust-lang.org/beta/std/sync/atomic/enum.Ordering.html
+        let id = AUTO_INC.fetch_add(1, Ordering::SeqCst);
+        println!("Creating City #{}", id);
+
         Ok(Json(json!({
             "id": id,
             "name": format!("City #{}", id)

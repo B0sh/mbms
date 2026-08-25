0551f84ec45d7758ac2bd4b6fbf94394369469ba
B0sh <b0shbbl@hotmail.com>
2026-08-24 19:18:56 -0500

    More trials

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use axum::{
-    Json, Router, http::StatusCode, routing::get,
+    Json, Router, extract::Path, http::StatusCode, routing::{get, post},
 };
 use serde_json::{Value, json};
+
+#[derive(Deserialize)]
+struct CreateCityPayload {
+    city_name: String
+}
+
+static mut GLOBAL_ID: i32 = 0;
 
 #[tokio::main]
 async fn main() {
@@ -12,6 +19,12 @@ async fn main() {
         .route("/hello", get(hello))
         .route("/data", get(data))
         .route("/bad", get(bad_request))
+        .route("/city", post({
+            move |body| create_city(body)
+        }))
+        .route("/city/{id}", get({
+            move |path| get_city(path)
+        }))
     ;
 
     // run our app with hyper, listening globally on port 3000
@@ -29,4 +42,27 @@ async fn bad_request() -> Result<String, StatusCode> {
 
 async fn data() -> Json<Value> {
     Json(json!({ "data": 42 }))
+}
+
+async fn get_city(Path(city_id): Path<String>) -> Json<Value> {
+    Json(json!({
+        "id": city_id,
+        "name": format!("City #{}", city_id)
+    }))
+}
+
+async fn create_city(Json(payload): Json<CreateCityPayload>) -> Result<Json<Value>, StatusCode> {
+    if payload.city_name.len() > 8 {
+        Err(StatusCode::BAD_REQUEST)
+    }
+    else {
+        unsafe {
+            let id = GLOBAL_ID;
+            GLOBAL_ID += 1;
+        }
+        Ok(Json(json!({
+            "id": id,
+            "name": format!("City #{}", id)
+        })))
+    }
 }
